777978bd427a450bd8caa4da27681d898458202d
Gabriel Trindade <141684234+gabrieltfd2@users.noreply.github.com>
2024-05-01 21:50:57 -0300

    realizando tarefa

---
+++ src/components/Cabecalho/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import Cores from '../../cores'
+
+export const Cabecalho = styled.div`
+  background-color: ${Cores.corSecundaria};
+  color: ${Cores.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
+
